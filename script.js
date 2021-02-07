e86ee9ab89957d7859b0537d05e6b2114b4d5905
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-02-06 18:58:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ $(document).ready(function(){
     $("#panel").slideDown("fast");
   });
 });
+<style> 
+#panel, #flip {
+  padding: 5px;
+  text-align: center;
+  background-color: #e5eecc;
+  border: solid 1px #c3c3c3;
+}
+
+#panel {
+  padding: 50px;
+  display: none;
+}
